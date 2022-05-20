d7daa9d0bf0ba9856ea8b064b3f3cf11e333e909
kledson CR <kledson.rabelo.pb@compasso.com.br>
2022-05-20 01:09:50 -0300

    feat: criando mesnsagem e adicionando herança

--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -1,13 +1,9 @@
-export class NegociacoesView {
+import { Negociacoes } from "../models/negociacoes.js";
+import { View } from './view.js';
 
-    private elemento: HTMLElement;
+export class NegociacoesView extends View {
 
-    constructor(seletor: string) {
-
-        this.elemento = document.querySelector(seletor);
-    }
-
-    template(): string {
+    template(model: Negociacoes): string {
         return `
         <table class="table table-hover teble-bordered">
           <thead>
@@ -18,13 +14,24 @@ export class NegociacoesView {
             </tr>
         </thead>
         <tbody>
-    </tbody>
+        ${model.lista().map(negociacao => {
+            return `
+                <tr>
+                    <td>${new Intl.DateTimeFormat().format(negociacao.data)}</td>
+                    <td>${negociacao.quantidade}</td>
+                    <td>${negociacao.valor}</td>
+                </tr>
+            `;
+        }).join('')}         
+       </tbody>
     </table>            
         `;
     }
 
-    update(): void {
-        this.elemento.innerHTML = this.template();
+    update(model: Negociacoes): void {
+        const template = this.template(model);
+        console.log(template);
+        this.elemento.innerHTML = template;
     }
 
 }
--- dist/js/views/negociacoes-view.js
+++ dist/js/views/negociacoes-view.js
@@ -1,8 +1,6 @@
-export class NegociacoesView {
-    constructor(seletor) {
-        this.elemento = document.querySelector(seletor);
-    }
-    template() {
+import { View } from './view.js';
+export class NegociacoesView extends View {
+    template(model) {
         return `
         <table class="table table-hover teble-bordered">
           <thead>
@@ -13,11 +11,22 @@ export class NegociacoesView {
             </tr>
         </thead>
         <tbody>
-    </tbody>
+        ${model.lista().map(negociacao => {
+            return `
+                <tr>
+                    <td>${new Intl.DateTimeFormat().format(negociacao.data)}</td>
+                    <td>${negociacao.quantidade}</td>
+                    <td>${negociacao.valor}</td>
+                </tr>
+            `;
+        }).join('')}         
+       </tbody>
     </table>            
         `;
     }
-    update() {
-        this.elemento.innerHTML = this.template();
+    update(model) {
+        const template = this.template(model);
+        console.log(template);
+        this.elemento.innerHTML = template;
     }
 }
